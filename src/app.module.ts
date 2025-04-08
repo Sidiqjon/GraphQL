75d30c25972931a1d12df7277cad4a3d57aff487
Sidiqjon <sidiqjonyusufjanov7@gmail.com>
2025-04-08 12:35:54 +0500

    Finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ProductModule } from './product/product.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+      debug: true,
+    }),
+    PrismaModule,
+    ProductModule,
+  ],
 })
 export class AppModule {}
---
+++ src/product/dto/update-product.input.ts
@@ -0,0 +1,9 @@
+import { CreateProductInput } from './create-product.input';
+import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+
+@InputType()
+export class UpdateProductInput extends PartialType(CreateProductInput) {
+  @Field(() => Int)
+  id: number;
+}
+
